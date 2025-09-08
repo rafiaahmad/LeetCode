629735cdae522010c6217e3d333d3957cdafce94
Rafia Ahmad <ahmadrafiya82@gmail.com>
2025-09-08 22:41:19 +0530

    Update 0719-find-k-th-smallest-pair-distance.java

--- 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.java
+++ 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.java
@@ -1,5 +1,21 @@
 class Solution {
     public int smallestDistancePair(int[] nums, int k) {
+//         //Brute Force Approach
+//         List <Integer> distances = new ArrayList<>();
+
+//         //generate all possible pairs and calculate distances
+//         for(int i=0; i<nums.length; i++){
+//             for(int j = i+1; j<nums.length; j++){
+//                 distances.add(Math.abs(nums[i] - nums[j]));
+//             }
+//         }
+//         //sort the list of distances
+//         Collections.sort(distances);
+
+//         //return the k-1th index
+//         return (distances.get(k-1));
+//     }
+// }
         Arrays.sort(nums); // sort the array
 
         int low = 0, high = nums[nums.length-1] - nums[0];
