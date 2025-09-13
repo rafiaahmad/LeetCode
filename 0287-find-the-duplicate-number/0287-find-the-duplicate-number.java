b60338cd654338f1e131783eaa84fdbb3a093940
Rafia Ahmad <ahmadrafiya82@gmail.com>
2025-09-13 23:33:43 +0530

    Time: 5 ms (65.3%), Space: 58.4 MB (47.88%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,25 +1,22 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        // Approach1: Binary Search
-        int low = 1, high = nums.length - 1;
-        int duplicate = -1;
+        // Approach2: Floyd Detection Algorith (Slow and fast pointer)
+        // Step 1: Find intersection
+        int slow = nums[0];
+        int fast = nums[0];
         
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            
-            // count numbers <= mid
-            int count = 0;
-            for (int num : nums) {
-                if (num <= mid) count++;
-            }
-            
-            if (count > mid) {
-                duplicate = mid;
-                high = mid - 1; // search left half
-            } else {
-                low = mid + 1; // search right half
-            }
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+        
+        // Step 2: Find cycle entrance
+        slow = nums[0];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
         }
-        return duplicate;
+        
+        return slow; // or fast
     }
 }
