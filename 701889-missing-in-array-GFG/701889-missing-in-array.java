3e192ed4f04794a5377255afadb614f66389de12
Rafia Ahmad <ahmadrafiya82@gmail.com>
2025-09-13 12:29:56 +0530

    Added solution - LeetHub

--- 701889-missing-in-array-GFG/701889-missing-in-array.java
+++ 701889-missing-in-array-GFG/701889-missing-in-array.java
@@ -13,11 +13,17 @@ class Solution {
         int n = arr.length+1; //One number is missing
         int xor = 0;
         
-        //XOR all elements from 1..n
-        for(int i=1; i<=n; i++) xor ^= i;
+        // //XOR all elements from 1..n
+        // for(int i=1; i<=n; i++) xor ^= i;
         
-        //XOR all elements in the array
-        for(int num : arr) xor ^= num;
+        // //XOR all elements in the array
+        // for(int num : arr) xor ^= num;
+        for (int i = 0; i < arr.length; i++) {
+            xor ^= arr[i];       // XOR array element
+            xor ^= (i + 1);     // XOR number from 1..n-1
+        }
+
+        xor ^= n; // XOR the last number 'n'
         
         return xor; // The result is the missing number
     }
