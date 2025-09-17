07c8b4ba32ed2493b4d6276cbfb872096fad9203
Rafia Ahmad <ahmadrafiya82@gmail.com>
2025-09-17 21:45:09 +0530

    Added solution - LeetHub

--- 703853-lcm-and-gcd-GFG/703853-lcm-and-gcd.java
+++ 703853-lcm-and-gcd-GFG/703853-lcm-and-gcd.java
@@ -1,19 +1,14 @@
 class Solution {
     public static int[] lcmAndGcd(int a, int b) {
         // code here
-        int ori_a = a, ori_b = b;
-        //Find GCD sing Euclidean Algo
-        // 
-        while(b>0){
-            int tmp = a%b;
-            a = b;
-            b = tmp;
-        }
-        int gcd = a;
-        
-        //calculate lcm = (a*b)/ gcd;
-        int lcm = (ori_a*ori_b)/gcd;
+        int gcd = findGCD(a,b);
+        int lcm = (a*b)/gcd;
         
         return new int[]{lcm, gcd};
+    }
+    
+    static int findGCD(int a, int b){
+        if(b==0) return a;
+        return findGCD(b, a%b);
     }
 }
