3109a9d1dcca2d409d25437981c8412e1aef4d03
Rafia Ahmad <ahmadrafiya82@gmail.com>
2025-09-17 21:39:33 +0530

    Added solution - LeetHub

---
+++ 703853-lcm-and-gcd-GFG/703853-lcm-and-gcd.java
@@ -0,0 +1,17 @@
+class Solution {
+    public static int[] lcmAndGcd(int a, int b) {
+        // code here
+        int ori_a = a, ori_b = b;
+        while(b>0){
+            int tmp = a%b;
+            a = b;
+            b = tmp;
+        }
+        int gcd = a;
+        
+        //calculate lcm = (a*b)/ gcd;
+        int lcm = (ori_a*ori_b)/gcd;
+        
+        return new int[]{lcm, gcd};
+    }
+}
