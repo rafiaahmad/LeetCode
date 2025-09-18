45e69d04c3bea1940d6e8e327efecfa5530f444b
Rafia Ahmad <ahmadrafiya82@gmail.com>
2025-09-18 23:54:57 +0530

    Time: 0 ms (100%), Space: 40.9 MB (73.08%) - LeetHub

--- 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.java
+++ 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.java
@@ -1,17 +1,13 @@
 class Solution {
     public int maxChunksToSorted(int[] arr) {
-        int n = arr.length;
         int chunks = 0;
+        int maxSoFar = 0;
 
-        for (int i = 0; i < n; i++) {
-            int maxVal = arr[0];
-            boolean isChunk = true;
-
-            for (int j = 0; j <= i; j++) {
-                maxVal = Math.max(maxVal, arr[j]);
-            }
-
-            if (maxVal == i) {
+        for (int i = 0; i < arr.length; i++) {
+            maxSoFar = Math.max(maxSoFar, arr[i]);
+            
+            // if max element seen so far == index, we can cut here
+            if (maxSoFar == i) {
                 chunks++;
             }
         }
