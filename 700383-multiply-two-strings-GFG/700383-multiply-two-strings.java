53d1a80105e1e77a522560110390de51d103bf4e
Rafia Ahmad <ahmadrafiya82@gmail.com>
2025-09-20 02:17:25 +0530

    Added solution - LeetHub

--- 700383-multiply-two-strings-GFG/700383-multiply-two-strings.java
+++ 700383-multiply-two-strings-GFG/700383-multiply-two-strings.java
@@ -1,83 +1,70 @@
 class Solution {
     public String multiplyStrings(String s1, String s2) {
-        // code here
-        int n1 = s1.length(), n2 = s2.length();
-        if (n1 == 0 || n2 == 0)
-            return "0";
         
-        // check if string are negative
-        int nn = 1, mm = 1;
-        if (s1.charAt(0) == '-')
-            nn = -1;
-        if (s2.charAt(0) == '-')
-            mm = -1;
+        //check the string is positive or negative
+        boolean neg1 = (s1.charAt(0) == '-');
+        //remove negative sign for calculation
+        if(neg1) s1 = s1.substring(1);
         
-        int isNeg = nn * mm;
+        boolean neg2 = (s2.charAt(0) == '-');
+        //remove negative sign for calculation
+        if(neg2) s2 = s2.substring(1);
         
-        // will keep the result number in 
-        // vector in reverse order
+        //remove leading zeros
+        s1 = removeLeadingZeroes(s1);
+        s2 = removeLeadingZeroes(s2);
+        
+        // If either is zero, return "0"
+        if (s1.equals("0") || s2.equals("0")) return "0";
+        
+        //check the result will be positive or negative
+        boolean resultNegative = (neg1 ^ neg2);
+        
+        //array to store the result
+        int n1 = s1.length();
+        int n2 = s2.length();
         int[] result = new int[n1 + n2];
         
-        // index by s1
-        int i1 = 0;
-        
-        // index by s2
-        int i2 = 0;
-        
-        // go from right to left by s1
-        for (int i = n1 - 1; i >= 0; i--) {
+        //go from right to left for string s1
+        for(int i = n1-1; i>=0; i--){
             
-            if (s1.charAt(i) == '-')
-                continue;
-            int carry = 0;
+            //take the value of string ith character into integer
             int n1Digit = s1.charAt(i) - '0';
-            i2 = 0;
             
-            // go from right to left by s2
-            for (int j = n2 - 1; j >= 0; j--) {
-                if (s2.charAt(j) == '-')
-                    continue;
+            //go from right to left for string s2
+            for(int j=n2-1; j>=0; j--){
+
+                //take the value of string ith character into integer
                 int n2Digit = s2.charAt(j) - '0';
                 
-                // multiply and add this result 
-                // to the existing result
-                int sum = n1Digit * n2Digit + result[i1 + i2] + carry;
+                //Store the multiple, plus previous result if available
+                int sum = n1Digit*n2Digit + result[i+j+1];
                 
-                // carry for next iteration
-                carry = sum / 10;
+                //Store the result in Least Significant
+                result[i+j+1] = sum % 10;
                 
-                // store result
-                result[i1 + i2] = sum % 10;
-                
-                i2++;
+                //Store carry before result index
+                result[i+j] += sum / 10;
             }
-            
-            // store carry in next cell
-            if (carry > 0)
-                result[i1 + i2] += carry;
-            
-            i1++;
         }
         
-        // ignore '0's from the right
-        int i = result.length - 1;
-        while (i >= 0 && result[i] == 0)
-            i--;
+        //convert result array to String
+        StringBuilder res = new StringBuilder();
+        for(int r : result){
+            if(!(res.length() == 0 && r == 0))
+                res.append(r);
+        }
         
-        // if all were '0's - means either 
-        // both or one of s1 or s2 were '0'
-        if (i == -1)
-            return "0";
+         // Add negative sign if needed
+        if (resultNegative) res.insert(0, '-');
         
-        // generate the result string
-        String s = "";
-        while (i >= 0)
-            s += Integer.toString(result[i--]);
-        
-        // if negative
-        if (isNeg == -1)
-            s = "-" + s;
-        
-        return s;
+        // conver res to string and return result
+        return res.toString();
+    }
+    
+    public String removeLeadingZeroes(String s){
+        int i = 0;
+        while(i<s.length() && s.charAt(i) == '0') i++;
+        return (i == s.length()) ? "0" : s.substring(i);
     }
 }
