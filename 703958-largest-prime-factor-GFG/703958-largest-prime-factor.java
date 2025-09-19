fe1f112b0b4ec09ecb08985ca577936bf4da8f59
Rafia Ahmad <ahmadrafiya82@gmail.com>
2025-09-19 22:09:09 +0530

    Added solution - LeetHub

--- 703958-largest-prime-factor-GFG/703958-largest-prime-factor.java
+++ 703958-largest-prime-factor-GFG/703958-largest-prime-factor.java
@@ -3,40 +3,27 @@
 class Solution {
     static int largestPrimeFactor(int n) {
         // code here
-        int sqrt = (int) Math.sqrt(n);
-        if(isPrime(n)) return n;
+        int maxPrime = -1;
         
-        int greatPrime = 2;
-        for(int i = 2; i<=sqrt; i++){
-            if(n%i == 0)
-            {
-                if(isPrime(i))
-                   greatPrime = Math.max(greatPrime, i) ;
+        //remove all even factors
+        while(n%2 == 0){
+            maxPrime = 2;
+            n /=2;
+        }
+        
+        //check odd factors
+        for(int i = 3; i*i<=n; i +=2){
+            while(n%i == 0){
+                maxPrime = i;
+                n /=i;
             }
         }
         
-        for(int j = sqrt; j>0; j--){
-            if(n%j == 0)
-            {
-                if(isPrime(n/j))
-                   greatPrime = Math.max(greatPrime, n/j) ;
-            }
+        //check even if n>2, then n itself is Prime
+        if(n>2){
+            maxPrime = n;
         }
         
-        return greatPrime;
-    }
-    
-    static boolean isPrime(int n){
-        if(n<2) return false;
-        if(n==2) return true;
-        if(n%2 == 0) return false;
-        int i=3;
-        
-        while(i*i <=n){
-            if(n%i == 0)
-                return false;
-            i += 2;
-        }
-        return true;
+        return maxPrime;
     }
 }
