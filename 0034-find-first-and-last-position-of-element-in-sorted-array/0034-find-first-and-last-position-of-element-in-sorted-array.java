59a669df4eb09c375e99a4bffc09bb26576d1d3e
Rafia Ahmad <ahmadrafiya82@gmail.com>
2025-09-22 20:52:28 +0530

    Time: 0 ms (100%), Space: 46 MB (49.24%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,35 +1,37 @@
 class Solution {
-    //function searchRange
     public int[] searchRange(int[] nums, int target) {
-        int first = -1;
-        int last = -1;
-        // find index in 1st half
-        int start = 0, end = nums.length - 1;
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            if (target == nums[mid]) {
+        int first = -1, last = -1;
+        int start = 0, end = nums.length-1;
+
+        //find the first occurence -- once target found, end = target and 
+        // search the  target further in left side
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            if(nums[mid] == target){
                 first = mid;
-                end = mid - 1;
-            } else if (target > nums[mid]) {
-                start = mid + 1;
-            } else {
-                end = mid - 1;
+                end = mid-1;
             }
+            else if( nums[mid] > target)
+                end = mid-1;
+            else
+                start = mid +1;
+        }
+        //find the last occurence -- once target found, start = target index+1 
+        // and search the target in the right side
+
+        start = 0; end = nums.length-1;
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            if(nums[mid] == target){
+                last = mid;
+                start = mid+1;
+                }
+            else if( nums[mid] > target)
+                end = mid-1;
+            else
+                start = mid +1;
         }
 
-        start = 0;
-        end = nums.length - 1;
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            if (target == nums[mid]) {
-                last = mid;
-                start = mid + 1;
-            } else if (target > nums[mid]) {
-                start = mid + 1;
-            } else {
-                end = mid - 1;
-            }
-        }
-        return new int[] { first, last };
+        return new int[] {first, last};
     }
 }
