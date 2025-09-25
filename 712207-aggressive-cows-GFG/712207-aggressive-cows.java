299a62d13de5b8b3828a53cac6eb43e1593fa908
Rafia Ahmad <ahmadrafiya82@gmail.com>
2025-09-25 23:45:56 +0530

    Added solution - LeetHub

--- 712207-aggressive-cows-GFG/712207-aggressive-cows.java
+++ 712207-aggressive-cows-GFG/712207-aggressive-cows.java
@@ -1,46 +1,42 @@
 class Solution {
     public int aggressiveCows(int[] stalls, int k) {
-        // sorting the array to ensure stalls in sequence
+        //sort the stalls
         Arrays.sort(stalls);
-        int res = 0;
+        int res = 0; //minimum distance assume zero initially
         
-         // Search Space for Binary Search
-         int low = 1;
-         int high = stalls[stalls.length -1] - stalls[0];
-         
-         while(low <= high){
-             int mid = low + (high-low)/2;
-             
-            // If the mid distance is possible, update
-            // the result and search for larger distance
-             if(check(stalls, k, mid)){
-                 res = mid;
-                 low = mid+1;
-             } else{
-                 high = mid-1;
-             }
-         }
-         return res;
-    }
-    
-    // function to check if we can place k cows
-    // with at least dist distance apart
-    static boolean check (int[] stalls, int k, int dist){
-        int cnt = 1; //place 1st count at 0th index stalls[0]
-        int prev = stalls[0];
-        
-        
-        for(int i=1; i<stalls.length; i++){
-            // if the current stall is at least dist away
-            // from the previous one place the cow here
-            if(stalls[i] - prev >= dist){
-            prev = stalls[i];
-            cnt++;
+        //Apply Binary search
+        int low = 1, high = stalls[stalls.length-1]-stalls[0];
+        while(low<=high){
             
-            if(cnt == k) return true;
+            int mid = low + (high-low)/2;
+            //if mid distance is possible for all cows to allocate
+            //then search for larger distance
+            if(check(stalls, k, mid)){
+                res = mid;
+                low = mid + 1;
+            } else{
+                high = mid - 1;
             }
         }
-        // return true if we are able to place all 'k' cows
-        return false;
+        
+        return res;
+    }
+    
+    //function to check if we can place k cows with atleast dist aprart or not
+    static boolean check(int[] stalls, int k, int dist){
+        int cnt = 1,
+            prev = stalls[0];
+        
+        for(int i = 1; i<stalls.length; i++){
+            if(stalls[i]-prev >= dist){
+                prev = stalls[i];
+                cnt++;
+            }
+            
+            //check if count equals k return true -- Early exit
+            if( cnt == k) return true;
+        }
+        
+        return false; //otherwise return false in last
     }
 }
