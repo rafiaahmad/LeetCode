b2effffc04d224be70a524735b05b5fae7cb9052
Rafia Ahmad <ahmadrafiya82@gmail.com>
2025-09-26 22:06:04 +0530

    Added solution - LeetHub

--- 701723-allocate-minimum-pages-GFG/701723-allocate-minimum-pages.java
+++ 701723-allocate-minimum-pages-GFG/701723-allocate-minimum-pages.java
@@ -1,51 +1,46 @@
 class Solution {
     public int findPages(int[] arr, int k) {
-        // if number of students are more than total books
-        // then allocation is not possible
-        if(k > arr.length) return -1;
+        //return if its not possible to allocate books to all students
+        if(arr.length < k) return -1;
         
-        //Search space for Binary Search
-        int lo = Arrays.stream(arr).max().getAsInt();
-        int hi = Arrays.stream(arr).sum();
-        int res = -1; //maximum no. of pages 
+        //Search space for Binary search
+        int max = Arrays.stream(arr).max().getAsInt();
+        int sum = Arrays.stream(arr).sum();
+        int res = -1;
         
-        while(lo<=hi){
-            int pageLimit = lo + (hi-lo)/2;
+        int low = max;
+        int high = sum;
+        
+        while(low<=high){
+            int mid = low + (high - low)/2;
             
-            if(isValid(arr, k, pageLimit)){
-                res = pageLimit;
-                hi = pageLimit - 1;
+            // Search if possible to allocate all books among k student 
+            // with atmost k #of pages
+            if(isValid(arr, k, mid)){
+                res = mid;
+                high = mid - 1;
             } else{
-                lo = pageLimit + 1;
+                low = mid + 1;
             }
         }
         return res;
     }
     
-    
-    // function to check if books can be allocated to
-    // all k students without exceeding 'pageLimit'
-    static boolean isValid(int[] arr, int k, int pageLimit){
-        
-         // starting from the first student
-        int pageSum = 0;
-        int cntStudent = 1;
-        
+    static boolean isValid(int[] arr, int k, int page){
+        int cnt = 1;
+        int sumPage = 0;
         
         for(int i = 0; i<arr.length; i++){
-        
-        // if adding the current book exceeds the page 
-        // limit, assign the book to the next student
-        if(pageSum + arr[i] > pageLimit){
-            cntStudent++;
-            pageSum = arr[i];
-        } else{
-            pageSum += arr[i];
+           
+            if(sumPage + arr[i] > page){
+                cnt++;
+                sumPage = arr[i];
+            } else{
+                sumPage += arr[i];
+            }
+            if(cnt > k) return false;
         }
-        
-        //early exit if count of student becomes more than k
-        // if(cntStudent > k) return false;
-        }
-        return (cntStudent <=k);
+        // return (cnt<=k);
+        return true;
     }
 }
