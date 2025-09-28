e947bb3bb82044c4cee5231b84b848a65fa3458d
Rafia Ahmad <ahmadrafiya82@gmail.com>
2025-09-28 15:30:16 +0530

    Time: 9 ms (96.12%), Space: 40.5 MB (47.49%) - LeetHub

--- 0668-kth-smallest-number-in-multiplication-table/0668-kth-smallest-number-in-multiplication-table.java
+++ 0668-kth-smallest-number-in-multiplication-table/0668-kth-smallest-number-in-multiplication-table.java
@@ -1,37 +1,22 @@
 class Solution {
     public int findKthNumber(int m, int n, int k) {
-        int low = 1, high = m*n;
-        int ans = 0;
+        int min = 1, max = m*n;
 
-        //Search space for binary search
-        while(low <= high){
-        int mid = low + (high-low)/2;
+        while(min<max){
+            int mid = min + (max-min)/2;
+            int count = bsCountLess(mid, m, n);
 
-        if(countLessOrEqual(mid, k, m, n)){
-            ans = mid;
-            high = mid - 1;
-        } else
-            low = mid + 1;
+            if(count>=k) max = mid;
+            else min = mid+1;
         }
-
-        // return low; //or high ans converge at this point
-        return ans; //or high ans converge at this point
+        return min;
     }
 
-    public boolean countLessOrEqual(int target, int k, int m, int n){
-        int cnt = 0;
-        int row = 1, col = n;
-
-        //search space for counting elements <= target
-        while(row <= m && col >= 1){
-            if(row*col > target)
-                col--; // Skip all ele. of current col. > target
-            else{
-                cnt += col; //count all el. of current row <= target
-                row++; //proceed in next column
-            }
+    static int bsCountLess(int mid, int m, int n){
+        int count = 0;
+        for(int i=1; i<=m; i++){
+            count += Math.min(n, mid/i);
         }
-
-        return (cnt>=k);
+        return count;
     }
 }
