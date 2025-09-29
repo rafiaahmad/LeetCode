f31b788b815b0fb69120ad982274b955fd42265d
Rafia Ahmad <ahmadrafiya82@gmail.com>
2025-09-29 22:16:59 +0530

    Time: 9 ms (96.15%), Space: 40.8 MB (24.04%) - LeetHub

--- 0668-kth-smallest-number-in-multiplication-table/0668-kth-smallest-number-in-multiplication-table.java
+++ 0668-kth-smallest-number-in-multiplication-table/0668-kth-smallest-number-in-multiplication-table.java
@@ -7,26 +7,15 @@ class Solution {
         int mid = low + (high-low)/2;
 
         if(countLessOrEqual(mid, k, m, n))
-            high = mid;
+            high = mid; //Search kth smallest <= mid
         else
-            low = mid + 1;
+            low = mid + 1; //Search Kth smalles > mid
         }
         return low; //or high ans converge at this point
     }
 
     public boolean countLessOrEqual(int target, int k, int m, int n){
         int cnt = 0;
-        // int row = 1, col = n;
-
-        // //search space for counting elements <= target
-        // while(row <= m && col >= 1){
-        //     if(row*col > target)
-        //         col--; // Skip all ele. of current col. > target
-        //     else{
-        //         cnt += col; //count all el. of current row <= target
-        //         row++; //proceed in next column
-        //     }
-        // }
         for(int i=1; i<=m; i++){
             cnt += Math.min(n, target/i);
         }
