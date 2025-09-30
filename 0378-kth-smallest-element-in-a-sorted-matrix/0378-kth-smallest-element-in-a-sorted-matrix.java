07c67c6cd8f07246e1dc5b0bc3fdace73773d43b
Rafia Ahmad <ahmadrafiya82@gmail.com>
2025-09-30 21:43:22 +0530

    Time: 0 ms (100%), Space: 50 MB (28.59%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
@@ -2,40 +2,35 @@ class Solution {
     public int kthSmallest(int[][] matrix, int k) {
         
         int n = matrix.length;
-        int ans = Integer.MIN_VALUE;
-        int low = matrix[0][0];
-        int high = matrix[n-1][n-1];
+        int min = matrix[0][0], max = matrix[n-1][n-1];
+        
+        while(min < max){
+            int mid = min + (max-min)/2;
+            int count = BSCountLess(matrix, mid, n);
 
-        while(low<=high){
-            int mid = low + (high-low)/2;
-
-            if(countLessOrEqual(matrix, mid, n, k)){
-                ans = mid;
-                high = mid-1; // search for next smaller mid possible
+            if(count >= k){
+                max = mid;
             } else{
-                low = mid+1; // search for higher mid which is possible smallest
+                min = mid+1;
             }
         }
+        return min;
+    }
 
-        return ans;
-    }   
-
-    // Function to check whether mid is the possible Kth smallest
-    public boolean countLessOrEqual(int[][] matrix, int target, int n, int k){
+    int BSCountLess(int[][] matrix, int target, int n){
         int count = 0;
-        int row = 0; int col = n-1;
+        int row = n-1; //Start frm botton left
+        int col = 0;  
 
-        while(row < n && col>=0){
-            if(matrix[row][col] > target){
-                //all ele. in this column are > target
-                col--;
-            } else if(matrix[row][col] <= target){
-                //all element in row are <= target
-                count += col + 1; //count them
-                row++; //proceed to next row
+        while(row >=0 && col < n){
+            if(matrix[row][col] <= target){
+                //all element in current column upto this row are <=target
+                count += row + 1;
+                col++;
+            } else{
+                row--; // all element in this row will be greater than target
             }
         }
-
-        return (count>=k);
+        return count;
     }
 }
