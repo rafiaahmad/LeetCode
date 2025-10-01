0428e12af34ea969530466c6b6fbe6fd6d76716d
Rafia Ahmad <ahmadrafiya82@gmail.com>
2025-10-01 18:57:58 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.java
+++ 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.java
@@ -1,43 +1,18 @@
 class Solution {
     public int smallestDistancePair(int[] nums, int k) {
-//         //Brute Force Approach
-//         List <Integer> distances = new ArrayList<>();
+        List <Integer> distances = new ArrayList<>();
 
-//         //generate all possible pairs and calculate distances
-//         for(int i=0; i<nums.length; i++){
-//             for(int j = i+1; j<nums.length; j++){
-//                 distances.add(Math.abs(nums[i] - nums[j]));
-//             }
-//         }
-//         //sort the list of distances
-//         Collections.sort(distances);
-
-//         //return the k-1th index
-//         return (distances.get(k-1));
-//     }
-// }
-        Arrays.sort(nums); // sort the array
-
-        int low = 0, high = nums[nums.length-1] - nums[0];
-
-        while(low<high){
-            int mid = low + (high-low)/2;
-
-            //count how many pairs <=mid
-            int count = 0, j=0;
-            for(int i = 0; i<nums.length; i++){
-                while(j<nums.length && nums[j]-nums[i] <=mid){
-                    j++;
-                }
-                count += (j-i-1);
-            }
-
-            if(count >=k){ // mid is big enough
-                high = mid;
-            } else{
-                low = mid+1; // need bigger distance
+        //generate all possible pairs and calculate distances
+        for(int i=0; i<nums.length; i++){
+            for(int j = i+1; j<nums.length; j++){
+                distances.add(Math.abs(nums[i] - nums[j]));
             }
         }
-        return low;
+
+        //sort the list of distances
+        Collections.sort(distances);
+
+        //return the k-1th index
+        return (distances.get(k-1));
     }
 }
