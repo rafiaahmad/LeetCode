df347d38263ab683be333986d6fa2704b4c4781c
Rafia Ahmad <ahmadrafiya82@gmail.com>
2025-10-02 23:22:35 +0530

    Added solution - LeetHub

--- 493-sorting-elements-of-an-array-by-frequency-GFG/493-sorting-elements-of-an-array-by-frequency.java
+++ 493-sorting-elements-of-an-array-by-frequency-GFG/493-sorting-elements-of-an-array-by-frequency.java
@@ -1,46 +1,46 @@
 import java.util.*; 
 public class Main {
     public static void main(String[] args) {
-        // Your code here
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt(); // number of test cases
         
-        while (T-- > 0) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt(); // Number of Test Cases
+        
+        while(T-- > 0){
             int N = sc.nextInt(); // size of array
             int[] arr = new int[N];
-            for (int i = 0; i < N; i++) {
+            
+            //Step0: Input original array
+            for(int i = 0; i < N; i++){
                 arr[i] = sc.nextInt();
             }
             
-            // Step 1: Count frequency
+            //Step 1: Store frequency
             HashMap<Integer, Integer> freq = new HashMap<>();
-            for (int x : arr) {
+            for(int x : arr)
                 freq.put(x, freq.getOrDefault(x, 0) + 1);
-            }
             
-            // Step 2: Put array elements into list for custom sorting
+            //Step 2: Put the array element into list for custom sorting
             List<Integer> list = new ArrayList<>();
-            for (int x : arr) list.add(x);
-            
-            // Step 3: Sort by frequency, then by value
-            Collections.sort(list, (a, b) -> {
-                int fa = freq.get(a);
-                int fb = freq.get(b);
+            for(int x : arr)
+                list.add(x);
                 
-                if (fa != fb) {
-                    return fb - fa; // higher frequency first
-                } else {
-                    return a - b;   // smaller element first
-                }
+            //Step 3: Sort the elements by frequency, then by value
+            Collections.sort(list, (a,b) -> {
+               int fa = freq.get(a);
+               int fb = freq.get(b);
+               
+               if(fa != fb)
+                    return fb-fa;
+                else
+                    return a-b;
             });
             
-            // Step 4: Print result
-            for (int x : list) {
+            //Step 4: Print result
+            for(int x : list){
                 System.out.print(x + " ");
             }
             System.out.println();
         }
-        
         sc.close();
     }
 }
