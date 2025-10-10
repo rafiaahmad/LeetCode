f06405f828b601009368ce8dcdc9c391d7d55fca
Rafia Ahmad <ahmadrafiya82@gmail.com>
2025-10-10 21:52:48 +0530

    Time: 0 ms (100%), Space: 43.4 MB (11.55%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,40 +1,44 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-         int n = nums.length;
-        
-        // Step 1: Find the first "dip" (pivot) from the right
-        int i = n - 2;
-        while (i >= 0 && nums[i] >= nums[i + 1]) {
-            i--;
-        }
+        int n = nums.length;
 
-        // Step 2: If pivot exists, find element just larger than nums[i]
-        if (i >= 0) {
-            int j = n - 1;
-            while (nums[j] <= nums[i]) {
-                j--;
+        // Step 1: Find break point
+        int  i = n-2; 
+
+        //traverse array from last compare adjacent until decrement start
+        while(i >=0 && nums[i] >= nums[i+1]){
+                i--;
             }
-            // Swap pivot with that element
-            swap(nums, i, j);
+        //i return the breakpoint index
+
+        //Step 2: Find ele. just > ele. of breakpoint index
+        if(i>=0){
+            int j = n-1;
+            while(nums[j] <= nums[i] )
+                j--;
+            //Step 3: Swap ele. of index start with minIndex
+            swapElement(nums, i, j);
         }
 
-        // Step 3: Reverse the suffix (right side after pivot)
-        reverse(nums, i + 1, n - 1);
+        //Step4: Reverse the ele. of array after breakpoint index after swap
+        reverseArray(nums, i+1, n-1);
+
+        //Here we have the array having next permutation
     }
 
-    // Helper: swap two elements
-    private void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
+    public void swapElement(int[] nums, int index1, int index2){
+        int tmp = nums[index1];
+        nums[index1] = nums[index2];
+        nums[index2] = tmp;
     }
 
-    // Helper: reverse array from left to right
-    private void reverse(int[] nums, int left, int right) {
-        while (left < right) {
-            swap(nums, left, right);
-            left++;
-            right--;
+    public void reverseArray(int[] nums, int start, int end){
+        
+
+        while(start<end){
+            swapElement(nums, start, end);
+            start++;
+            end--;
         }
     }
 }
