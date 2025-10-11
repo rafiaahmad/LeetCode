52f20b372036996347dac9209b33fbc29b8f4add
Rafia Ahmad <ahmadrafiya82@gmail.com>
2025-10-11 15:17:07 +0530

    Added solution - LeetHub

--- 701198-wave-array-GFG/701198-wave-array.java
+++ 701198-wave-array-GFG/701198-wave-array.java
@@ -1,24 +1,15 @@
 class Solution {
     public void sortInWave(int arr[]) {
-        // Approach3: O(N) solution - Optimal without sorting
-        int n = arr.length;
-        
-        for(int i=0; i<n; i +=2){
-            // If current even index is not the first element
-            // and previous element is greater than current
-            if(i>0 && arr[i-1] > arr[i]) 
-                swap(arr, i-1, i);
-                
-            // If current even index is not the last element
-            // and next element is greater than current
-            if(i<n-1 && arr[i] < arr[i+1])
-                swap(arr, i, i+1);
+        for(int i = 1; i<arr.length; i += 2){ 
+          int tmp = arr[i];
+          arr[i] = arr[i-1];
+          arr[i-1] = tmp;
         }
     }
     
-    public void swap(int[] arr, int i, int j){
-        int temp    = arr[i];
-            arr[i]  = arr[j];
-            arr[j]  = temp;
-    }
+    // private void swap(int a , int b){
+    //     a = a+b;
+    //     b = a-b;
+    //     a = a-b;
+    // }
 }
