2b93481f89e67e9895359c9491722d9ded770139
Rafia Ahmad <ahmadrafiya82@gmail.com>
2025-10-11 15:53:57 +0530

    Added solution - LeetHub

--- 701203-equilibrium-point-GFG/701203-equilibrium-point.java
+++ 701203-equilibrium-point-GFG/701203-equilibrium-point.java
@@ -1,24 +1,20 @@
 class Solution {
     // Function to find equilibrium point in the array.
     public static int findEquilibrium(int arr[]) {
-        int n = arr.length;
-        
-        // PreSum: sum of elements before index i
-        int PreSum = arr[0];
-        
-        // PostSum: sum of elements after index i (starting from index 2)
-        int PostSum = 0;
-        for(int i=2; i<n; i++) PostSum += arr[i];
-        
-        // Check equilibrium from index 1 to n-2
-        for(int i=1; i<n-1; i++){
-            if(PreSum == PostSum) 
-                return i; // equilibrium index (0-based)
-            else{
-                PreSum += arr[i];       // expand left sum
-                PostSum -= arr[i+1];    // shrink right sum
-            }
+        //Build prefix
+        int[] prefix = new int[arr.length];
+        prefix[0] = arr[0];
+        for(int i = 1; i<arr.length; i++)
+            prefix[i] = prefix[i-1] + arr[i];
+            
+        //tarverse array from length 1..last ele. and check the equilibrium point
+        for(int i = 1; i<arr.length; i++){
+            //sum of ele. i+1 ... arr.length-1
+            int postSum = prefix[arr.length - 1] - prefix[i];
+            if(postSum == prefix[i-1])
+                return i;
         }
-        return -1; //No equiblrium found
+        
+        return -1;
     }
 }
