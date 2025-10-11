cbe4b510493532a1be79871df24e08477151d0dc
Rafia Ahmad <ahmadrafiya82@gmail.com>
2025-10-11 17:45:18 +0530

    Time: 0 ms (100%), Space: 42 MB (8.12%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.java
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.java
@@ -3,20 +3,18 @@ class Solution {
         int maxDepth = 0;
         int cnt = 0;
 
-        Stack<Character> stack = new Stack();
-
         for(int i = 0; i<s.length(); i++){
             char ch = s.charAt(i);
 
             if(ch == '(' ){
-                stack.push(ch);
+                // stack.push(ch);
                 cnt++;
                 maxDepth = Math.max(maxDepth, cnt);
             } else if( ch == ')'){
-                if(!stack.isEmpty()){
-                    stack.pop();
+                // if(!stack.isEmpty()){
+                    // stack.pop();
                     cnt--;
-                }                    
+                // }                    
             }
         }
 
