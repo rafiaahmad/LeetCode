12990002fb7bba84c9d40dea32b3a64a11bb8f31
Rafia Ahmad <ahmadrafiya82@gmail.com>
2025-10-12 11:48:40 +0530

    Time: 7 ms (18.38%), Space: 45 MB (17.71%) - LeetHub

--- 0768-max-chunks-to-make-sorted-ii/0768-max-chunks-to-make-sorted-ii.java
+++ 0768-max-chunks-to-make-sorted-ii/0768-max-chunks-to-make-sorted-ii.java
@@ -1,32 +1,19 @@
 class Solution {
     public int maxChunksToSorted(int[] arr) {
-        int n = arr.length;
-
-        //Step 1 : Copy and sort the array
         int[] sorted = arr.clone();
         Arrays.sort(sorted);
-
-        //Step 2 : Hashmap to count frequency of numbers seen so far
-        HashMap<Integer, Integer> freqOriginal = new HashMap<>();
-        HashMap<Integer, Integer> freqSorted = new HashMap<>();
-
+        
+        Map<Integer, Integer> count = new HashMap<>();
         int chunks = 0;
 
-        //Step 3 : Traverse both arrays simultaneously
-        for(int i = 0; i<arr.length; i++){
+        for (int i = 0; i < arr.length; i++) {
+            count.put(arr[i], count.getOrDefault(arr[i], 0) + 1);
+            if (count.get(arr[i]) == 0) count.remove(arr[i]);
 
-            //Update frequency for original arr[i]
-            freqOriginal.put(arr[i], freqOriginal.getOrDefault(arr[i], 0) + 1);
-            if(freqOriginal.get(arr[i]) == 0)
-                freqOriginal.remove(arr[i]);
+            count.put(sorted[i], count.getOrDefault(sorted[i], 0) - 1);
+            if (count.get(sorted[i]) == 0) count.remove(sorted[i]);
 
-            //Update frequency for sorted array
-            freqSorted.put(sorted[i], freqSorted.getOrDefault(sorted[i], 0) + 1);
-            if(freqSorted.get(sorted[i]) == 0)
-                freqSorted.remove(sorted[i]);
-
-            //Step 4 : compare both frequency maps and count chunk
-            if(freqOriginal.equals(freqSorted))
+            if (count.isEmpty())
                 chunks++;
         }
         return chunks;
