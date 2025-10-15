ea5c736ab80e19e9c80854b9430d68967e97895f
Rafia Ahmad <ahmadrafiya82@gmail.com>
2025-10-15 11:43:15 +0530

    Added solution - LeetHub

--- 702706-sum-of-subarrays-GFG/702706-sum-of-subarrays.java
+++ 702706-sum-of-subarrays-GFG/702706-sum-of-subarrays.java
@@ -1,8 +1,11 @@
 class Solution {
     public int subarraySum(int[] arr) {
+        
+        //Approach 3 : Mathematical formaula
         int sum = 0; 
         int n = arr.length;
         for(int i = 0; i<n; i++){
+            // (n-1)*(i+1) times arr[i] appears in all possible subarrays
             sum += (n-i)*(i+1)*arr[i];
         }
         
