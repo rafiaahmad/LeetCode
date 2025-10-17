8f69efddbf64c9c7bb816bb48600ea06f910562d
Rafia Ahmad <ahmadrafiya82@gmail.com>
2025-10-18 00:10:22 +0530

    Time: 3 ms (56.71%), Space: 41.7 MB (93.75%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for(int  i = 0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if(ch == '(' || ch == '{' || ch == '[')
+                stack.push(ch);
+            if(ch == ')' ){
+                if(!stack.isEmpty() && stack.peek() == '(')
+                    stack.pop();
+                else
+                    return false;
+            }
+
+            if(ch == '}' ){
+                if(!stack.isEmpty() && stack.peek() == '{')
+                    stack.pop();
+                else
+                    return false;
+            }
+
+            if(ch == ']' ){
+                if(!stack.isEmpty() && stack.peek() == '[')
+                    stack.pop();
+                else
+                    return false;
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
