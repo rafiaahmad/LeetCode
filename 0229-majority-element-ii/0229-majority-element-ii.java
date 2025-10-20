c5356d6f182b3a1f42c226e7573b5bba29139177
Rafia Ahmad <ahmadrafiya82@gmail.com>
2025-10-20 12:50:35 +0530

    Time: 14 ms (40.36%), Space: 50.8 MB (9.51%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,40 +1,18 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
+        HashMap<Integer, Integer> freq = new HashMap<>();
         List<Integer> result = new ArrayList<>();
-        if (nums == null || nums.length == 0) return result;
+
+        //Step 1 : Count frequency of each element
+        for(int x : nums)
+            freq.put(x, freq.getOrDefault(x, 0) + 1);
         
-        // Step 1: Find potential candidates
-        int candidate1 = 0, candidate2 = 1; // initialize with different numbers
-        int count1 = 0, count2 = 0;
-        
-        for (int num : nums) {
-            if (num == candidate1) {
-                count1++;
-            } else if (num == candidate2) {
-                count2++;
-            } else if (count1 == 0) {
-                candidate1 = num;
-                count1 = 1;
-            } else if (count2 == 0) {
-                candidate2 = num;
-                count2 = 1;
-            } else {
-                count1--;
-                count2--;
-            }
+        //Step 2 : Check freq > n/3 times
+        for(Map.Entry<Integer, Integer> entry : freq.entrySet()){
+            if(entry.getValue() > nums.length/3)
+                result.add(entry.getKey());
         }
 
-        // Step 2: Verify actual counts
-        count1 = 0;
-        count2 = 0;
-        for (int num : nums) {
-            if (num == candidate1) count1++;
-            else if (num == candidate2) count2++;
-        }
-        
-        if (count1 > nums.length / 3) result.add(candidate1);
-        if (count2 > nums.length / 3) result.add(candidate2);
-        
         return result;
     }
 }
