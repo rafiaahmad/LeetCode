c63f8074b1de1caa2b5633f3943c91f48a6efb84
Rafia Ahmad <ahmadrafiya82@gmail.com>
2025-10-21 23:44:14 +0530

    Added solution - LeetHub

--- 701205-rearrange-array-alternately-GFG/701205-rearrange-array-alternately.java
+++ 701205-rearrange-array-alternately-GFG/701205-rearrange-array-alternately.java
@@ -1,11 +1,16 @@
 class Solution {
     public void rearrange(int arr[]) {
-        // code here
+        // Approach 2 : Sortinng -- Encoding -- Two-pointer
+        // Space: O(1), Time: O(n log n)
+        // Step 1 : Sort the array
         Arrays.sort(arr);
         int n =  arr.length;
+        
+        // Step 2 : Take 2 pointers
         int minIndex = 0, maxIndex = n-1;
         int maxElement = arr[n-1]+1;
         
+        // Step 3 : Traverse the array, do encoding and store answers
         for(int i = 0; i<n; i++){
             if(i%2 == 0){
                 //Even index --> place maximum
@@ -18,6 +23,7 @@ class Solution {
             }
         }
         
+        // Step 4 : Decode the array to get the original re-arranged array
         for (int i = 0; i < n; i++){
             arr[i] = arr[i] / maxElement;
         }
