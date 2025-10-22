bb78e18bea5af104973b9c0421e126cc3e992fea
Rafia Ahmad <ahmadrafiya82@gmail.com>
2025-10-22 22:57:03 +0530

    Time: 18 ms (24.8%), Space: 45.7 MB (68.02%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,25 +1,24 @@
 class Solution {
     public String frequencySort(String s) {
-        // Step 1: Count frequency of each character
+        // Step 1 : Count the frequency using HashMap
         HashMap<Character, Integer> freq = new HashMap<>();
-        for (char c : s.toCharArray()) {
-            freq.put(c, freq.getOrDefault(c, 0) + 1);
-        }
+        for(char ch : s.toCharArray())
+            freq.put(ch, freq.getOrDefault(ch, 0) + 1);
 
-        // Step 2: Put all characters into a list
+        // Step 2 : Create list of entries and sort 
         List<Character> list = new ArrayList<>(freq.keySet());
 
-        // Step 3: Sort characters by frequency (descending)
+        //Step 3 : Sort the list
         Collections.sort(list, (a, b) -> freq.get(b) - freq.get(a));
 
-        // Step 4: Build result string
-        StringBuilder sb = new StringBuilder();
-        for (char c : list) {
-            int count = freq.get(c);
-            for (int i = 0; i < count; i++) {
-                sb.append(c);
-            }
+        // Step 4 : Build result Strin and  Append into string
+        StringBuilder result = new StringBuilder();
+        for(int i = 0; i < list.size(); i++){
+            for(int j = 0; j<freq.get(list.get(i)); j++)
+                result.append(list.get(i)); //append the character as its frequency
         }
-        return sb.toString();
+
+        //Step 5 : Return the string
+        return result.toString();
     }
 }
