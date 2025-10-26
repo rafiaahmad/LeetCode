ca6fcabadd4e7cc18d807a436dcfc1ecad854cda
Rafia Ahmad <ahmadrafiya82@gmail.com>
2025-10-26 15:54:54 +0530

    Time: 0 ms (100%), Space: 42.2 MB (61.83%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,5 +1,6 @@
 class Solution {
     public double myPow(double x, int n) {
+        // TC: O(log N), S.c : O(1)
         long N = n; // Use long to handle Integer.MIN_VALUE 
         if (N < 0) { 
             x = 1 / x; 
@@ -9,11 +10,11 @@ class Solution {
         double ans = 1; 
         
         while (N > 0) {
-            if ((N & 1) == 1) {
-                 ans *= x; } 
-            x *= x; 
-            N >>= 1; 
+            if ((N & 1) == 1) { // 1010 & 0001 = 100 // 101 & 001 == 1 // 10&01 = 0 // 1&1 =1
+                 ans *= x; } // 4 // 2^2 * 2^8 = 2^10 = 1024
+            x *= x; // 16 // 256 // 2^20
+            N >>= 1; // 5 - 2 - 0
         } 
-        return ans;
+        return ans; //1024
     }
 }
