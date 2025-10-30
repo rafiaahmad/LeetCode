08c1065b6a84cc2b4451e5424518bbd568b7aa89
Rafia Ahmad <ahmadrafiya82@gmail.com>
2025-10-30 20:52:27 +0530

    Time: 7 ms (92.37%), Space: 48.5 MB (5.69%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,24 +1,46 @@
 class Solution {
     public String frequencySort(String s) {
-        // Step 1 : Count the frequency using HashMap
-        HashMap<Character, Integer> freq = new HashMap<>();
-        for(char ch : s.toCharArray())
-            freq.put(ch, freq.getOrDefault(ch, 0) + 1);
 
-        // Step 2 : Create list of entries and sort 
-        List<Character> list = new ArrayList<>(freq.keySet());
+        int[] freq = new int[62]; // for frequency counting
+        for(char ch : s.toCharArray()){
+            // Step 1 : Map frequencies
+            int index;
+            if(ch >= '0' && ch <= '9') index = ch - '0';
+            else if(ch >= 'a' && ch <= 'z') index = ch - 'a' + 36;
+            else index = ch - 'A' + 10;
 
-        //Step 3 : Sort the list
-        Collections.sort(list, (a, b) -> freq.get(b) - freq.get(a));
-
-        // Step 4 : Build result Strin and  Append into string
-        StringBuilder result = new StringBuilder();
-        for(int i = 0; i < list.size(); i++){
-            for(int j = 0; j<freq.get(list.get(i)); j++)
-                result.append(list.get(i)); //append the character as its frequency
+            // Step 2 : count the frequency
+            freq[index]++;
         }
 
-        //Step 5 : Return the string
-        return result.toString();
+        // Step 3 : Prepare bucket
+        int n = s.length();
+        List<Character>[] bucket = new List[n+1];
+
+        for(int i = 0; i<=n; i++)
+            bucket[i] = new ArrayList<>();
+
+        //Add characters to bukcet
+        for(int i = 0; i<62; i++){
+            if(freq[i] > 0)
+                bucket[freq[i]].add(charForIndex(i)); // convert index back to char
+        }
+
+        // Step 4 : Prepare result
+        StringBuilder result = new StringBuilder();
+        for(int f = n; f>=1; f--){
+            for(char ch : bucket[f]){
+                for(int k = 0; k<f; k++)
+                    result.append(ch);
+            }
+        }
+
+        return result.toString(); // return result
+    }
+
+    public char charForIndex(int index){
+        if(index < 10) return (char)('0' + index);
+        else if(index < 36) return (char) ('A' + (index - 10));
+        else return (char)('a' + (index - 36));
     }
 }
