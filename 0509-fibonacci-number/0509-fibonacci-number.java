e9d1413eea9465012667596953ef9d44339f1e6a
Rafia Ahmad <ahmadrafiya82@gmail.com>
2025-11-01 22:09:33 +0530

    Time: 8 ms (43.19%), Space: 40.4 MB (52.26%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,13 +1,6 @@
 class Solution {
     public int fib(int n) {
-        if(n== 0) return 0;
-        if (n==1) return 1;
-        return fibonacci(n-1) + fibonacci(n-2);
-    }
-
-    private int fibonacci(int n){
-        if(n== 0) return 0;
-        if (n==1) return 1;
-        return fibonacci(n-1) + fibonacci(n-2);
+        if(n<=1) return n;
+        return fib(n-1) + fib(n-2);
     }
 }
