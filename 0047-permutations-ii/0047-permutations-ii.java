b194b856da6f8f3fee453502415bc854f57bc213
Rafia Ahmad <ahmadrafiya82@gmail.com>
2025-11-12 08:38:07 +0530

    Time: 4 ms (29.84%), Space: 46.8 MB (6.19%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.java
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -1,30 +1,40 @@
 class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
-        Arrays.sort(nums);
+        Arrays.sort(nums); // sort to group duplicates
 
         List<List<Integer>> res = new ArrayList<>();
+
+        //Array to keep track of used elements of the array
         boolean[] used = new boolean[nums.length];
+
+        //Recursive function
         generate(nums, used, new ArrayList<>(), res);
         return res;
     }
 
     private void generate(int[] nums, boolean[] used, List<Integer> current, List<List<Integer>> res){
-        //Base case
+        //Base case : when current size == array size
         if(current.size() == nums.length){
             res.add(new ArrayList<>(current));
             return;
         }
             
-
+        //Looping through the choices
         for(int i = 0; i<nums.length; i++){
+            //Skip already used elements
             if(used[i]) continue;
 
+            // Avoid duplicate: Only pick first unused element
             if(i>0 && nums[i] == nums[i-1] && (!used[i-1])) continue;
 
+            //Choose: Mark as used and add into current list
             used[i] = true;
             current.add(nums[i]);
+
+            //recursive function to build next possible permutation
             generate(nums, used, current, res);
 
+            //Backtracking : Un-Choose
             used[i] = false;
             current.remove(current.size() -1);
         }
