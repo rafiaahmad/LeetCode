f16c283536a4b094f5e738c56e68b2f8c0d251ca
Rafia Ahmad <ahmadrafiya82@gmail.com>
2025-11-14 19:26:04 +0530

    Time: 0 ms (100%), Space: 42.3 MB (5.18%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,18 +1,14 @@
 class Solution {
-    int[] dp;
     public int climbStairs(int n) {
-        // Method 2: Recursion + Memoization (Top-Down DP)
-        dp = new int[n+1];
-        Arrays.fill(dp, -1);
-
-        return solve(n);
-    }
-
-    private int solve(int n){
+        // Method 3: Tabulation (Bottom-Up DP)
         if(n<=2) return n;
-        if(dp[n] != -1) return dp[n];
 
-        dp[n] = solve(n-1) + solve(n-2);
+        int[] dp = new int[n+1];
+        dp[1] = 1; dp[2] = 2;
+
+        for(int i = 3; i<=n; i++ )
+            dp[i] = dp[i-1] + dp[i-2];
+
         return dp[n];
     }
 }
