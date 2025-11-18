7ac6f874ebaae90ba6a568ceb8b58b9996ca8d77
Rafia Ahmad <ahmadrafiya82@gmail.com>
2025-11-18 22:37:54 +0530

    Added solution - LeetHub

--- 703092-two-sum-pair-with-given-sum-GFG/703092-two-sum-pair-with-given-sum.java
+++ 703092-two-sum-pair-with-given-sum-GFG/703092-two-sum-pair-with-given-sum.java
@@ -1,14 +1,14 @@
 class Solution {
     boolean twoSum(int arr[], int target) {
         
-        //Approach 1 : TC: O(n^2), SC: O(1)
+        //Approach 1 : TC: O(n + n log n), SC: O(1)
         int n = arr.length;
         if(n==1) return false;
         
-        Arrays.sort(arr);
+        Arrays.sort(arr); // O(n log n)
         
         int left = 0, right = n-1;
-        while(left<right){
+        while(left<right){ // O(n)
             int sum = arr[left] + arr[right];
             
             if(sum == target)
