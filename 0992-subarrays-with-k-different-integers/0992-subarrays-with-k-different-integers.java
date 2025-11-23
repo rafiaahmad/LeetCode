7a61f3f1173e433aec2afb40354d713e30c729f1
Rafia Ahmad <ahmadrafiya82@gmail.com>
2025-11-23 21:05:24 +0530

    Time: 5 ms (98.58%), Space: 49.3 MB (24.11%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
@@ -1,27 +1,28 @@
 class Solution {
     public int subarraysWithKDistinct(int[] nums, int k) {
-        if(k > nums.length) return 0;
-
-        return atMost(nums, k) - atMost(nums, k-1);
+        return atMost(nums, k) - atMost(nums, k - 1);
     }
 
-    private int atMost(int[] nums, int k){
-        int left = 0; int count = 0;
-        HashMap<Integer, Integer> freq = new HashMap<>();
+    private int atMost(int[] nums, int k) {
+        int n = nums.length;
+        int[] freq = new int[n + 1];
+        int left = 0, count = 0;
 
-        for(int right = 0; right<nums.length; right++){
-            freq.put(nums[right], freq.getOrDefault(nums[right], 0) + 1);
+        for (int right = 0; right < n; right++) {
+            // Add nums[right]
+            if (freq[nums[right]] == 0) 
+                k--;  
+            freq[nums[right]]++;
 
-            if(freq.get(nums[right]) == 1)
-                k--;
-
-            while(k<0){
-                freq.put(nums[left], freq.get(nums[left])-1);
-                if(freq.get(nums[left]) == 0)
+            // Shrink until <= k distinct
+            while (k < 0) {
+                freq[nums[left]]--;
+                if (freq[nums[left]] == 0)
                     k++;
-                left++;                
+                left++;
             }
 
+            // Add number of subarrays ending at right
             count += right - left + 1;
         }
 
