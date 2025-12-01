cb97203200a6878112b235e69cdd765086c2a803
Rafia Ahmad <ahmadrafiya82@gmail.com>
2025-12-01 22:11:32 +0530

    Time: 5 ms (77.85%), Space: 77.5 MB (5.51%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,15 +1,16 @@
 class Solution {
     public int maxArea(int[] height) {
         int n = height.length;
-        int left = 0, right = n-1;
+        int left = 0, right = n-1; // two pointers for deciding the 2 lines
         int maxArea = Integer.MIN_VALUE;
 
         while(left<right){
-            int width = right-left;
-            int high = Math.min(height[right], height[left]);
-            maxArea = Math.max(maxArea, width * high);
+            int width = right-left; // width
+            int high = Math.min(height[right], height[left]); //height
+            maxArea = Math.max(maxArea, width * high); // maxArea keep on calculating
 
-            if(height[left] < height[right])
+            // logic to shrink
+            if(height[left] < height[right]) 
                 left++;
             else
                 right--;
