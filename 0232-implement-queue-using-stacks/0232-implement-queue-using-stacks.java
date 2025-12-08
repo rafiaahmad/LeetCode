91b6270418684ee4b727300cc89a1fe6ad716e11
Rafia Ahmad <ahmadrafiya82@gmail.com>
2025-12-08 19:55:59 +0530

    Time: 0 ms (100%), Space: 42.4 MB (53.15%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -2,36 +2,30 @@ class MyQueue {
     Stack<Integer> inStack;
     Stack<Integer> outStack;
 
-    /** Constructor: initialize two stacks */
     public MyQueue() {
-        inStack  = new Stack<>();
+        inStack = new Stack<>();
         outStack = new Stack<>();
     }
     
-    /** Push element x to the back of queue */
     public void push(int x) {
         inStack.push(x);
     }
     
-    /** Removes the element from in front of queue and returns that element */
     public int pop() {
-        shiftStack();
+        shiftStacks();
         return outStack.pop();
     }
     
-    /** Get the front element */
     public int peek() {
-        shiftStack();
+        shiftStacks();
         return outStack.peek();
     }
-
-    /** Returns whether the queue is empty */ 
+    
     public boolean empty() {
         return inStack.isEmpty() && outStack.isEmpty();
     }
 
-    /** Helper method: transfer elements if output is empty */
-    public void shiftStack(){
+    public void shiftStacks(){
         if(outStack.isEmpty()){
             while(!inStack.isEmpty())
                 outStack.push(inStack.pop());
