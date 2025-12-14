e13697e60977f3bb0efe91fb908272bf78495c3b
Rafia Ahmad <ahmadrafiya82@gmail.com>
2025-12-14 19:11:13 +0530

    Added solution - LeetHub

--- 701343-next-greater-element-GFG/701343-next-greater-element.java
+++ 701343-next-greater-element-GFG/701343-next-greater-element.java
@@ -1,20 +1,26 @@
 class Solution {
     public ArrayList<Integer> nextLargerElement(int[] arr) {
         int n = arr.length;
-        ArrayList<Integer> res = new ArrayList<>();
-        Deque<Integer> st = new ArrayDeque<>();
+        ArrayList<Integer> res = new ArrayList<>(); // res list
+        Deque<Integer> st = new ArrayDeque<>(); // for building motonic stack
         
+        // Step 1 : Traverse the array from right to left
         for(int i = n-1; i>=0; i--){
+            // Step 2 : Pop element if current element >= stack top ele.
             while(!st.isEmpty() && st.peek() <= arr[i])
                 st.pop();
                 
+            // Step 3 : Decide Next greater element
             if(st.isEmpty())
-                res.add(-1);
+                res.add(-1); // stack is empty, no NGE 
             else
-                res.add(st.peek());
+                res.add(st.peek()); // NGE available
+            
+            // Step 4 : Push the current element into stack
             st.push(arr[i]);
         }
-        Collections.reverse(res);
+        
+        Collections.reverse(res); // reverse the result array
         return res;
     }
 }
