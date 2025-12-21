e4c5cd902c54898f07bfb9d7488bfd25745c72e2
Rafia Ahmad <ahmadrafiya82@gmail.com>
2025-12-21 21:24:28 +0530

    Time: 76 ms (63.49%), Space: 47.9 MB (52.38%) - LeetHub

---
+++ 2030-smallest-k-length-subsequence-with-occurrences-of-a-letter/2030-smallest-k-length-subsequence-with-occurrences-of-a-letter.java
@@ -0,0 +1,50 @@
+class Solution {
+    public String smallestSubsequence(String s, int k, char letter, int repetition) {
+
+        int n = s.length();
+        int remainingLetter = 0;
+
+        // Count total occurrences of `letter`
+        for (char c : s.toCharArray()) {
+            if (c == letter) remainingLetter++;
+        }
+
+        int usedLetter = 0;
+        Deque<Character> stack = new ArrayDeque<>();
+
+        for (int i = 0; i < n; i++) {
+            char curr = s.charAt(i);
+
+            // Try to pop for lexicographical order
+            while (!stack.isEmpty()
+                    && stack.peekLast() > curr
+                    && stack.size() - 1 + (n - i) >= k
+                    && (stack.peekLast() != letter || remainingLetter > repetition - usedLetter)) {
+
+                char removed = stack.pollLast();
+                if (removed == letter) usedLetter--;
+            }
+
+            // Try to push current character
+            if (stack.size() < k) {
+                if (curr == letter) {
+                    stack.addLast(curr);
+                    usedLetter++;
+                } else {
+                    // Ensure space for required letters
+                    if (k - stack.size() > repetition - usedLetter) {
+                        stack.addLast(curr);
+                    }
+                }
+            }
+
+            if (curr == letter) remainingLetter--;
+        }
+
+        // Build result
+        StringBuilder result = new StringBuilder();
+        for (char c : stack) result.append(c);
+
+        return result.toString();
+    }
+}
