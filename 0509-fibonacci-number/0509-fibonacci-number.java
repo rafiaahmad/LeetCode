780ac5edac92439ab138312672b4f6653fd7374a
Rafia Ahmad <ahmadrafiya82@gmail.com>
2026-01-01 23:30:19 +0530

    Time: 0 ms (100%), Space: 41.9 MB (69.81%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,17 +1,15 @@
 class Solution {
-    //Fibonacci with Memoization
-    int[] dp;
+    //Fibonacci with Tabulation : Bottom-UP Approach
     public int fib(int n) {
-        dp = new int[n+1];
-        Arrays.fill(dp, -1);
-        return solve(n);
-    }
 
-    private int solve(int n){
-        if(n<=1) return n;  //Base Case
-        if(dp[n] != -1) return dp[n];   //Already computed
+        if(n<=1) return n;
 
-        dp[n] = solve(n-1) + solve(n-2);
+        int[] dp = new int[n+1];
+        dp[0] = 0; dp[1] = 1;
+        
+        for(int i = 2; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
 
         return dp[n];
     }
