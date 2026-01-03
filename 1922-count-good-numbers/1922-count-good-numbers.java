6e5fc028549fa8b0407c8a7564d021628fd0384b
Rafia Ahmad <ahmadrafiya82@gmail.com>
2026-01-03 21:38:41 +0530

    Time: 0 ms (100%), Space: 42.4 MB (13.69%) - LeetHub

--- 1922-count-good-numbers/1922-count-good-numbers.java
+++ 1922-count-good-numbers/1922-count-good-numbers.java
@@ -8,10 +8,15 @@ class Solution {
     }
 
     private long power(long x, long n){
-        if(n==0) return 1;
-        long half = power(x, n/2) % mod;
-        long res = (half*half) % mod;
-
-        return (n%2 == 0)? res : (res*x) % mod;
+        long res = 1;
+        long mul = x;
+        while(n>0){
+            if(n%2 == 1)
+                res = (res*mul) % mod;
+            mul = (mul*mul) % mod;
+            n /= 2;
+        }
+        
+        return res;
     }
 }
