8893fb4fb69b847c50bc8a0ca3aeab8cafd02da5
Rafia Ahmad <ahmadrafiya82@gmail.com>
2026-01-19 10:50:31 +0530

    Time: 6 ms (83%), Space: 45.6 MB (73.54%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.java
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.java
@@ -1,29 +1,38 @@
 class Solution {
+    static class Pair{
+        int freq;
+        int value;
+
+        Pair(int freq, int value){
+            this.freq = freq;
+            this.value = value;
+        }
+    }
+
     public int[] frequencySort(int[] nums) {
         // Step 1 : Frequency Counting
         HashMap<Integer, Integer> freqMap = new HashMap<>();
         for(int num : nums)
             freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
 
-        // Step 2 : Create List of entries
-        List<Map.Entry<Integer, Integer>> list  = new ArrayList<>(freqMap.entrySet());
-
-        // Step 3 : Sort using comparator
-        list.sort((a,b) -> {
-            if(a.getValue() != b.getValue())
-                return a.getValue() - b.getValue(); // freq ↑
-            else
-                return b.getKey() - a.getKey();     // value ↓
-
+        // Step 2 : Build Heap (High Freq first --> Max Heap)
+        PriorityQueue<Pair> maxHeap = new PriorityQueue<>((a,b) -> {
+            if(a.freq != b.freq)
+                return a.freq - b.freq;
+            return b.value - a.value;
         });
 
-        // Step 4 : Build the sorted array
+        // Iterate over Map entry
+        for(Map.Entry<Integer, Integer> entry : freqMap.entrySet())
+            maxHeap.offer(new Pair(entry.getValue(), entry.getKey()));
+
+        // Step 3 : Build the sorted array
         int[] res = new int[nums.length];
         int k = 0;
-        for(int i = 0; i<list.size(); i++){
-            int freq = list.get(i).getValue();
-            for(int j = 0; j<freq; j++)
-                res[k++] = list.get(i).getKey();
+        while(!maxHeap.isEmpty()){
+            for(int i = 0; i<maxHeap.peek().freq; i++)
+                res[k++] = maxHeap.peek().value;
+            maxHeap.poll();
         }
 
         return res;
