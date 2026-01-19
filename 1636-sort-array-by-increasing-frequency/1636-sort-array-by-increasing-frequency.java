ed2e29f5e946755eb71003768b40aa662c17a846
Rafia Ahmad <ahmadrafiya82@gmail.com>
2026-01-19 11:11:53 +0530

    Time: 5 ms (95.19%), Space: 45.4 MB (85.68%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.java
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.java
@@ -15,24 +15,24 @@ class Solution {
         for(int num : nums)
             freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
 
-        // Step 2 : Build Heap (High Freq first --> Max Heap)
-        PriorityQueue<Pair> maxHeap = new PriorityQueue<>((a,b) -> {
+        // Step 2 : Build Heap (Low Freq first --> Min Heap)
+        PriorityQueue<Pair> minHeap = new PriorityQueue<>((a,b) -> {
             if(a.freq != b.freq)
-                return a.freq - b.freq; // frequency low
-            return b.value - a.value; // value high
+                return a.freq - b.freq; // Low Frequency First
+            return b.value - a.value; // High Value First
         });
 
         // Iterate over Map entry
         for(Map.Entry<Integer, Integer> entry : freqMap.entrySet())
-            maxHeap.offer(new Pair(entry.getValue(), entry.getKey()));
+            minHeap.offer(new Pair(entry.getValue(), entry.getKey()));
 
         // Step 3 : Build the sorted array
         int[] res = new int[nums.length];
         int k = 0;
-        while(!maxHeap.isEmpty()){
-            for(int i = 0; i<maxHeap.peek().freq; i++)
-                res[k++] = maxHeap.peek().value;
-            maxHeap.poll();
+        while(!minHeap.isEmpty()){
+            for(int i = 0; i<minHeap.peek().freq; i++)
+                res[k++] = minHeap.peek().value;
+            minHeap.poll();
         }
 
         return res;
