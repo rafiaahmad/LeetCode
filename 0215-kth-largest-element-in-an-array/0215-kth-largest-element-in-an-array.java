e98355ba61fb5a69fd6290f90078743888a4aa07
Rafia Ahmad <ahmadrafiya82@gmail.com>
2026-01-20 12:46:35 +0530

    Time: 69 ms (31.13%), Space: 69 MB (37.35%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,34 +1,15 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        //get minimum and maximum element of the array
-        int low = Arrays.stream(nums).min().getAsInt();
-        int high = Arrays.stream(nums).max().getAsInt();
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>();
+        int n = nums.length;
 
-        int ans = Integer.MAX_VALUE;
+        for(int i = 0; i<n; i++){
+            maxHeap.offer(nums[i]);
 
-        //Search space for binary search
-        while(low<=high){
-            int mid = low + (high-low)/2;
-
-            if(countGreaterOrEqual(nums, k, mid)){
-                ans = mid; // mid could be the possible answer
-                //Search for higher mid which will satisfy this condn.
-                low = mid + 1;
-            } else{
-                // search for lesser mid value
-                high = mid - 1;
-            }
+            if(maxHeap.size() > k)
+                maxHeap.poll();
         }
-        return ans;
-    }
 
-    public static boolean countGreaterOrEqual(int[] nums, int k, int mid){
-        int count = 0;
-
-        for(int num : nums){
-            if(num >= mid)
-                count++;
-        }
-        return (count>=k);
+        return maxHeap.peek();
     }
 }
