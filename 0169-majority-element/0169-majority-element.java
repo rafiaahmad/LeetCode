ed17fc614c0baf69c676c8028a8ff29b37b340a5
Rafia Ahmad <ahmadrafiya82@gmail.com>
2026-02-02 23:12:02 +0530

    Time: 2 ms (71.65%), Space: 55.6 MB (65.26%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,35 +1,15 @@
 class Solution {
-    public int majorityElement(int[] nums) {
-        // HashMap<Integer, Integer> freq = new HashMap<>();
-        // List<Integer> result = new ArrayList<>();
-        // int res = 0;
-
-        // //Step 1 : Count frequency of each element
-        // for(int x : nums)
-        //     freq.put(x, freq.getOrDefault(x, 0) + 1);
-        
-        // //Step 2 : Check freq > n/3 times
-        // for(Map.Entry<Integer, Integer> entry : freq.entrySet()){
-        //     if(entry.getValue() > nums.length/2)
-        //         // result.add(entry.getKey());
-        //         res = entry.getKey();
-        //         break;
-        // }
-
-        // return res;
-        // List<Integer> result = new ArrayList<>();
-        // if (nums == null || nums.length == 0) return result;
-        
+    public int majorityElement(int[] nums) {        
         // Step 1: Find potential candidates
         int candidate1 = 0, candidate2 = 1; // initialize with different numbers
         int count1 = 0, count2 = 0;
         
         for (int num : nums) {
-            if (num == candidate1) {
+            if (num == candidate1)
                 count1++;
-            } else if (num == candidate2) {
+            else if (num == candidate2)
                 count2++;
-            } else if (count1 == 0) {
+            else if (count1 == 0){
                 candidate1 = num;
                 count1 = 1;
             } else if (count2 == 0) {
