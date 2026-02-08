2ca168712ddb56e9b5b6e09f73d571381fbe7804
Rafia Ahmad <ahmadrafiya82@gmail.com>
2026-02-08 12:06:17 +0530

    Time: 4 ms (77.07%), Space: 94.5 MB (66.91%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -36,12 +36,17 @@ class Solution {
     }
 
     public ListNode reverse(ListNode head){
-        if(head == null || head.next == null) return head;
+        ListNode prev = null;
+        ListNode curr = head;
 
-        ListNode newHead = reverse(head.next);
-        head.next.next = head;
-        head.next = null;
+        while(curr != null){
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
 
-        return newHead;
+        head = prev;
+        return head;
     }
 }
