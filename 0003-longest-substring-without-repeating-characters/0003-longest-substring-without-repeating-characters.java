81e93047856edccf83e797bc6be7059576758bb7
Rafia Ahmad <ahmadrafiya82@gmail.com>
2026-02-17 19:17:16 +0530

    Time: 12 ms (12.28%), Space: 46.4 MB (47.7%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,37 +1,36 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        //Step 1: return string as it is length <=1
+        //Step 1: Early return case
         if(s.length() < 1) return s.length();
 
-        //Step 2 : Declare 2 pointers
+        //Step 2 : Left pointer, maxLen initialization
         int left = 0, maxLen = 0; //Initial window size 
 
+        //Step 3 : HashMap to store frequency
         HashMap<Character, Integer> freq = new HashMap<>();
-        // freq.put(s.charAt(left), freq.getOrDefault(s.charAt(left), 0)+1);
 
         for(int right = 0; right < s.length(); right++){
 
             char curr = s.charAt(right);
             
-            // Step 3 : Check if Valid Substring
+            // Step 4 : Checking for duplicate
             if(!freq.containsKey(curr)){
                 //right-left+1 : Unique Window size
                 maxLen = Math.max(maxLen, (right-left)+1);
                 freq.put(curr, freq.getOrDefault(curr, 0) + 1);
                 continue;
-            }
-            
+            }            
 
-            // Step 4 : Remove duplicates
+            // Step 5 : Shrinking the window
             while(freq.containsKey(curr)){
                 char removeChar = s.charAt(left);
-                left++; //window shrink
+                left++; 
                 freq.put(removeChar, freq.get(removeChar) - 1);
                 if(freq.get(removeChar) == 0)
                     freq.remove(removeChar); //reduce the frequency also
             }
 
-            //Step 5 : Add the frequency
+            //Step 5 : Adding the current character after shrink
             freq.put(curr, freq.getOrDefault(curr, 0) + 1);
         }
 
