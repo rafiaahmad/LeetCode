f630d37187010aa61faf23b38fc56d841f13fdd2
Rafia Ahmad <ahmadrafiya82@gmail.com>
2026-02-19 17:39:46 +0530

    Time: 4 ms (98.58%), Space: 53.6 MB (74.89%) - LeetHub

---
+++ 0089-gray-code/0089-gray-code.java
@@ -0,0 +1,13 @@
+class Solution {
+    public List<Integer> grayCode(int n) {
+        List<Integer> result = new ArrayList<>();
+        
+        int total = 1 << n;  // 2^n
+        
+        for(int i = 0; i < total; i++) {
+            result.add(i ^ (i >> 1));
+        }
+        
+        return result;
+    }
+}
