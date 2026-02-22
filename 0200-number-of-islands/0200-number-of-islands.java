ec7376e2999b68c274e369c0160d7f41ebdac7a5
Rafia Ahmad <ahmadrafiya82@gmail.com>
2026-02-22 11:26:04 +0530

    Time: 3 ms (87.48%), Space: 52.7 MB (22.04%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) return 0;
+        int rows = grid.length;
+        int cols = grid[0].length;
+        int islandCount = 0;
+
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (grid[r][c] == '1') {
+                    islandCount++;
+                    dfs(grid, r, c, rows, cols);
+                }
+            }
+        }
+        return islandCount;
+    }
+
+    private void dfs(char[][] grid, int r, int c, int rows, int cols) {
+        if (r < 0 || c < 0 || r >= rows || c >= cols || grid[r][c] == '0') {
+            return;
+        }
+        // Mark current land as visited
+        grid[r][c] = '0';
+
+        // Visit all 4 neighbors
+        dfs(grid, r + 1, c, rows, cols);
+        dfs(grid, r - 1, c, rows, cols);
+        dfs(grid, r, c + 1, rows, cols);
+        dfs(grid, r, c - 1, rows, cols);
+    }
+}
