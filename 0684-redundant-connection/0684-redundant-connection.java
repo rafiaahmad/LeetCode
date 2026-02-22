556710e746e7805b02aaa1572801b5584954eb92
Rafia Ahmad <ahmadrafiya82@gmail.com>
2026-02-22 11:37:03 +0530

    Time: 1 ms (80.06%), Space: 44.7 MB (83.38%) - LeetHub

---
+++ 0684-redundant-connection/0684-redundant-connection.java
@@ -0,0 +1,53 @@
+class Solution {
+    public int[] findRedundantConnection(int[][] edges) {
+        int n = edges.length;
+        UnionFind uf = new UnionFind(n + 1);
+
+        for (int[] edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+
+            if (uf.find(u) == uf.find(v)) {
+                return edge; // this edge creates a cycle
+            }
+            uf.union(u, v);
+        }
+        return new int[0];
+    }
+
+    class UnionFind {
+        int[] parent;
+        int[] rank;
+
+        UnionFind(int n) {
+            parent = new int[n];
+            rank = new int[n];
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
+                rank[i] = 0;
+            }
+        }
+
+        int find(int x) {
+            if (parent[x] != x) {
+                parent[x] = find(parent[x]);
+            }
+            return parent[x];
+        }
+
+        void union(int x, int y) {
+            int rootX = find(x);
+            int rootY = find(y);
+            if (rootX == rootY) return;
+
+            if (rank[rootX] < rank[rootY]) {
+                parent[rootX] = rootY;
+            } else if (rank[rootX] > rank[rootY]) {
+                parent[rootY] = rootX;
+            } else {
+                parent[rootY] = rootX;
+                rank[rootX]++;
+            }
+        }
+    }
+}
