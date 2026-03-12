4f74e2db2b26f072c954e710547426ec46ccfc51
Rafia Ahmad <ahmadrafiya82@gmail.com>
2026-03-12 11:10:29 +0530

    Time: 0 ms (100%), Space: 47.7 MB (68.22%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,30 +1,34 @@
 class Solution {
     public int trap(int[] height) {
 
-        int n = height.length;
+        int left = 0;
+        int right = height.length - 1;
+
+        int leftMax = 0;
+        int rightMax = 0;
+
         int water = 0;
 
-        Deque<Integer> st = new ArrayDeque<>();
+        while (left < right) {
 
-        for (int i = 0; i < n; i++) {
+            if (height[left] < height[right]) {
 
-            while (!st.isEmpty() && height[i] > height[st.peek()]) {
+                if (height[left] >= leftMax)
+                    leftMax = height[left];
+                else
+                    water += leftMax - height[left];
 
-                int mid = st.pop();
+                left++;
 
-                if (st.isEmpty()) break;
+            } else {
 
-                int left = st.peek();
+                if (height[right] >= rightMax)
+                    rightMax = height[right];
+                else
+                    water += rightMax - height[right];
 
-                int width = i - left - 1;
-
-                int boundedHeight =
-                        Math.min(height[left], height[i]) - height[mid];
-
-                water += width * boundedHeight;
+                right--;
             }
-
-            st.push(i);
         }
 
         return water;
