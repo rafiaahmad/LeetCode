bdf711ea889f69c560615ff0b03478edca5664d4
Rafia Ahmad <ahmadrafiya82@gmail.com>
2026-05-19 11:20:22 +0530

    Time: 10 ms (87.17%), Space: 47.2 MB (31.3%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 0462-minimum-moves-to-equal-array-elements-ii/0462-minimum-moves-to-equal-array-elements-ii.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minMoves2(int[] nums) {
+        Arrays.sort(nums);
+
+        int median = nums[nums.length / 2];
+
+        int moves = 0;
+
+        for (int num : nums) {
+            moves += Math.abs(num - median);
+        }
+
+        return moves;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
